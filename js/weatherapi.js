07577c49c4c1adb4a4042cd71f3ac05a43ecea0a
Dylan Shadbolt <jaemin1904@gmail.com>
2016-09-13 22:12:50 +1000

    Weather Api

---
+++ js/weatherapi.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('a').click(function(event) {
+    event.preventDefault();
+    $(this).hide("slow");
+  });
+
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Sydney,au&appid=784267ec0e780a59fcdfefbdde76a894&units=metric", function(result){
+            console.log(result);
+        });
+
+});
