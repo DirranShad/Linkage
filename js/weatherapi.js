4f59500467c93c09f65f9a97dce1ec3c58f8927f
Dylan Shadbolt <jaemin1904@gmail.com>
2016-09-13 22:40:01 +1000

    api

--- js/weatherapi.js
+++ js/weatherapi.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
     $(this).hide("slow");
   });
 
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Sydney,au&appid=784267ec0e780a59fcdfefbdde76a894&units=metric", function(result){
-            console.log(result);
+    $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=Sydney,au&appid=784267ec0e780a59fcdfefbdde76a894&units=metric", function(result){
+        console.log(result);
         });
 
 });
