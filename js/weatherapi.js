5a1ce7efca6543370b7a317520ff00c1a9d1e3b5
Dylan Shadbolt <jaemin1904@gmail.com>
2016-09-15 21:13:10 +1000

    New Weather Icons

--- js/weatherapi.js
+++ js/weatherapi.js
@@ -8,11 +8,13 @@ $.ajax({
   url: "https://api.forecast.io/forecast/02b0de8d458960b9309f89e3a3b73123/-33.865143,151.2099?units=si",
   dataType: "jsonp",
   success: function (apiresult) {
-      console.log('Weather was successfully received.');
+      console.log('Weather was successfully retrieved.');
       console.log(apiresult);
+      var weatherIcon = apiresult.currently.icon
+      var currentWeather = apiresult.currently.temperature
       $("#loading").css("display", "none");
-      $('#credit').before('<p id="weathertext">Sydney: <br> ' + apiresult.currently.temperature + '°c </p>');
-      $('')
+      $('#credit').before('<p id="weathertext">Sydney: <br> ' + currentWeather + '°c </p>');
+      $('#weathertext').before('<img id="weatherIcon" src="img/' + weatherIcon + '.svg">');
       $("body").fadeIn(1000);
   }
 });
