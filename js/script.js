c400b77e7b85a765e2e5835f73892a395c8b3b69
dirranshad <jaemin1904@gmail.com>
2016-09-15 17:21:19 +1000

    Robust Weather and improved display

--- js/script.js
+++ js/script.js
@@ -2,42 +2,44 @@ var tday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","
 var tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
 
 function GetClock(){
-var d=new Date();
-var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate();
-var nhour=d.getHours(),nmin=d.getMinutes(),ap;
+  var d=new Date();
+  var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate();
+  var nhour=d.getHours(),nmin=d.getMinutes(),ap;
 
-if(nhour==0){
-  ap=" AM";
-  nhour=12;
-}
-else if(nhour<12){
-  ap=" AM";
-  //document.getElementById('daytime').innerHTML=" Morning,";
-}
-else if(nhour==12){
-  ap=" PM";
-  document.getElementById('daytime').innerHTML=" Noon!";
-}
-else if(nhour>12){
-  ap=" PM";
-  nhour-=12;
-}
-else if((nhour>12)||(ap=" PM")){
-  document.getElementById('daytime').innerHTML=" Evening,";
-}
-else if((nhour<=6)||(ap=" PM")){
-  document.getElementById('daytime').innerHTML=" Afternoon,";
-}
-else if(ap=" AM"){
-  document.getElementById('daytime').innerHTML=" Morning,";
-}
+  if(nhour==0){
+    ap=" AM";
+    nhour=12;
+  }
+  else if(nhour<12){
+    ap=" AM";
+    //$('daytime').html=" Morning,";
+  }
+  else if(nhour==12){
+    ap=" PM";
+    $('#daytime').html(" Noon!");
+  }
+  else if(nhour>12){
+    ap=" PM";
+    nhour-=12;
+  }
+  else if((nhour<12)&&(ap==" PM")&&(nhour>6)){
+    $('#daytime').html(" Evening,");
+  }
+  else if((nhour<=6)&&(ap==" PM")){
+    $('#daytime').html(" Afternoon,");
+  }
+  else if(ap==" AM"){
+    $('#daytime').html(" Morning,");
+  }
 
-if(nmin<=9){nmin="0"+nmin;}
+  if(nmin<=9){nmin="0"+nmin;}
 
-document.getElementById('clockbox').innerHTML=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+", "+nhour+":"+nmin+ap+"";
+  $('#clockbox').html(""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+", "+nhour+":"+nmin+ap+"");
 }
 
 window.onload=function(){
-GetClock();
-setInterval(GetClock,1000);
+  GetClock();
+  setInterval(GetClock,1000);
 }
+
+console.log(new Date().toLocaleTimeString())
--- js/weatherapi.js
+++ js/weatherapi.js
@@ -4,10 +4,23 @@ $(document).ready(function() {
     $(this).hide("slow");
   });
 
+$.ajax({
+  url: "https://api.forecast.io/forecast/02b0de8d458960b9309f89e3a3b73123/-33.865143,151.2099?units=si",
+  dataType: "jsonp",
+  success: function (apiresult) {
+      console.log('here');
+      console.log(apiresult);
+      $("#loading").css("display", "none");
+      $('#weather').html('<p id="weathertext">Sydney: ' + apiresult.currently.temperature + '°c </p>');
+  }
+});
+
+});
+
+/*
 $.getJSON("https://api.forecast.io/forecast/02b0de8d458960b9309f89e3a3b73123/-33.865143,151.2099?units=si", function(apiresult){
     console.log(apiresult);
     $("#loading").css("display", "none");
     $('#weather').html('<p id="weathertext">Sydney: ' + apiresult.currently.temperature + '°c </p>');
     });
-
-});
+*/
